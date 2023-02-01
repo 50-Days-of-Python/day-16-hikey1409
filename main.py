e5fc7daf040f50aef5dd598e9e399b5bbb562b67
hikey1409 <101631288+hikey1409@users.noreply.github.com>
2023-02-01 22:07:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def sum_list(l):
   s = 0
-  
+  sdl=[]
+  for i in l:
+        if type(i) == list:
+            sdl.extend(flat_list(i))
+        else:
+            sdl.append(i)
+  for i in range(0,len(sdl)):
+    s=i+s
   # Write your logic here
   
   return s
